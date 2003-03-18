b073e9a480f4393a513b3b6efd1c847e5216cfd1
shahbaz.javeed <shahbaz.javeed>
2003-03-18 07:37:55 +0000

    * Added a new Permission to test with the /acl_two ACL * Added a new test that exercizes some (so far) unused areas of the acl hierarchy

--- Commons/src/java/test/com/netspective/commons/acl/AccessControlListTest.java
+++ Commons/src/java/test/com/netspective/commons/acl/AccessControlListTest.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: AccessControlListTest.java,v 1.5 2003-03-16 16:17:01 shahbaz.javeed Exp $
+ * $Id: AccessControlListTest.java,v 1.6 2003-03-18 07:37:55 shahbaz.javeed Exp $
  */
 
 package com.netspective.commons.acl;
@@ -102,21 +102,21 @@ public class AccessControlListTest extends TestCase
 
 		// Verify exactly _eleven_ permissions were loaded in this acl
 		Map aclPermissions = defaultAcl.getPermissionsByName();
-		assertEquals("Expected: Total permissions = 11, Found: Total permissions = " + aclPermissions.size(), 11, aclPermissions.size());
+		assertEquals("Expected: Total permissions = 10, Found: Total permissions = " + aclPermissions.size(), 10, aclPermissions.size());
 
 		// Verify the index of the /acl/role/normal-user permission is 10
 		Permission normalUser = defaultAcl.getPermission("/acl/role/normal-user");
-		assertEquals("Expected: Id for /acl/role/normal-user = 10, Found: " + normalUser.getId(), 10, normalUser.getId());
+		assertEquals("Expected: Id for /acl/role/normal-user = 9, Found: " + normalUser.getId(), 9, normalUser.getId());
 
 		// Verify the set of permissions for /acl/role/normal-user are exactly what we expect
 		BitSet normalUserPermissionSet = normalUser.getChildPermissions();
 		BitSet expectedPermissionSet = new BitSet(11);
+		expectedPermissionSet.set(1);
 		expectedPermissionSet.set(2);
 		expectedPermissionSet.set(3);
 		expectedPermissionSet.set(4);
 		expectedPermissionSet.set(5);
-		expectedPermissionSet.set(6);
-		expectedPermissionSet.set(10);
+		expectedPermissionSet.set(9);
 		assertEquals("Expected: Permissions for /acl/role/normal-user = " + expectedPermissionSet + ", Found: " + normalUserPermissionSet, expectedPermissionSet, normalUserPermissionSet);
 		assertEquals("Expected: Permissions for /acl/role/normal-user = " + expectedPermissionSet + ", Found: " + normalUserPermissionSet, expectedPermissionSet, normalUserPermissionSet);
 
@@ -153,7 +153,7 @@ public class AccessControlListTest extends TestCase
 		assertNotNull("Expected: AccessControlLists object, Found: null", acls);
 	    assertEquals("Expected: " + expectedNumACLs + " ACL, Found: " + acls.size(), expectedNumACLs.intValue(), acls.size());
 
-		// Verify the defaultAcl and the acl named "acl" are the same
+		// Verify there is no defaultAcl and that it returns a null
 	    AccessControlList defaultAcl = aclm.getDefaultAccessControlList();
 	    assertNull("Expected: Default ACL = null, Found: Non-Null ACL", defaultAcl);
 
@@ -178,11 +178,11 @@ public class AccessControlListTest extends TestCase
 		BitSet alternatePermissionSet = expectedPermission.getChildPermissions();
 		assertEquals("Expected: Permissions for /main/role/normal-user = " + normalUserPermissionSet + ", Found: " + alternatePermissionSet, normalUserPermissionSet, alternatePermissionSet);
 		BitSet expectedPermissionSet = new BitSet(11);
+		expectedPermissionSet.set(1);
 		expectedPermissionSet.set(2);
 		expectedPermissionSet.set(3);
 		expectedPermissionSet.set(4);
 		expectedPermissionSet.set(5);
-		expectedPermissionSet.set(6);
 		expectedPermissionSet.set(10);
 		assertEquals("Expected: Permissions for /main/role/normal-user = " + expectedPermissionSet + ", Found: " + normalUserPermissionSet, expectedPermissionSet, normalUserPermissionSet);
 		assertEquals("Expected: Permissions for /main/role/normal-user = " + expectedPermissionSet + ", Found: " + normalUserPermissionSet, expectedPermissionSet, normalUserPermissionSet);
@@ -238,47 +238,126 @@ public class AccessControlListTest extends TestCase
 		Map aclAclPermissions = aclAcl.getPermissionsByName();
 		Map aclTwoAclPermissions = aclTwoAcl.getPermissionsByName();
 		Map aclThreeAclPermissions = aclThreeAcl.getPermissionsByName();
-		assertEquals("Expected: Total permissions = 11, Found: Total permissions = " + aclAclPermissions.size(), 11, aclAclPermissions.size());
-		assertEquals("Expected: Total permissions = 9, Found: Total permissions = " + aclTwoAclPermissions.size(), 9, aclTwoAclPermissions.size());
-		assertEquals("Expected: Total permissions = 8, Found: Total permissions = " + aclThreeAclPermissions.size(), 8, aclThreeAclPermissions.size());
+		assertEquals("Expected: Total permissions = 10, Found: Total permissions = " + aclAclPermissions.size(), 10, aclAclPermissions.size());
+		assertEquals("Expected: Total permissions = 8, Found: Total permissions = " + aclTwoAclPermissions.size(), 8, aclTwoAclPermissions.size());
+		assertEquals("Expected: Total permissions = 7, Found: Total permissions = " + aclThreeAclPermissions.size(), 7, aclThreeAclPermissions.size());
 
 		// Verify the index of the /acl/role/normal-user permission is 10
 		Permission aclNormalUser = aclAcl.getPermission("/acl/role/normal-user");
 		Permission aclTwoNormalUser = aclTwoAcl.getPermission("/acl_two/role/normal-user");
 		Permission aclThreeReadOnlyUser = aclThreeAcl.getPermission("/acl_three/role/read-only-user");
-		assertEquals("Expected: Id for /acl/role/normal-user = 10, Found: " + aclNormalUser.getId(), 10, aclNormalUser.getId());
-		assertEquals("Expected: Id for /acl_two/role/normal-user = 19, Found: " + aclTwoNormalUser.getId(), 19, aclTwoNormalUser.getId());
-		assertEquals("Expected: Id for /acl_three/role/read-only-user = 27, Found: " + aclThreeReadOnlyUser.getId(), 27, aclThreeReadOnlyUser.getId());
+		assertEquals("Expected: Id for /acl/role/normal-user = 9, Found: " + aclNormalUser.getId(), 9, aclNormalUser.getId());
+		assertEquals("Expected: Id for /acl_two/role/normal-user = 17, Found: " + aclTwoNormalUser.getId(), 17, aclTwoNormalUser.getId());
+		assertEquals("Expected: Id for /acl_three/role/read-only-user = 24, Found: " + aclThreeReadOnlyUser.getId(), 24, aclThreeReadOnlyUser.getId());
 
 		// Verify the set of permissions for /acl/role/normal-user are exactly what we expect
 		BitSet aclNormalUserPermissionSet = aclNormalUser.getChildPermissions();
-		BitSet aclExpectedPermissionSet = new BitSet(27);
+		BitSet aclExpectedPermissionSet = new BitSet(24);
+		aclExpectedPermissionSet.set(1);
 		aclExpectedPermissionSet.set(2);
 		aclExpectedPermissionSet.set(3);
 		aclExpectedPermissionSet.set(4);
 		aclExpectedPermissionSet.set(5);
-		aclExpectedPermissionSet.set(6);
-		aclExpectedPermissionSet.set(10);
+		aclExpectedPermissionSet.set(9);
 		assertEquals("Expected: Permissions for /acl/role/normal-user = " + aclExpectedPermissionSet + ", Found: " + aclNormalUserPermissionSet, aclExpectedPermissionSet, aclNormalUserPermissionSet);
 
 		// Verify the set of permissions for /acl_two/role/normal-user are exactly what we expect
 		BitSet aclTwoNormalUserPermissionSet = aclTwoNormalUser.getChildPermissions();
-		BitSet aclTwoExpectedPermissionSet = new BitSet(27);
+		BitSet aclTwoExpectedPermissionSet = new BitSet(24);
+		aclTwoExpectedPermissionSet.set(11);
+		aclTwoExpectedPermissionSet.set(12);
 		aclTwoExpectedPermissionSet.set(13);
-		aclTwoExpectedPermissionSet.set(14);
-		aclTwoExpectedPermissionSet.set(15);
-		aclTwoExpectedPermissionSet.set(19);
+		aclTwoExpectedPermissionSet.set(17);
 		assertEquals("Expected: Permissions for /acl_two/role/normal-user = " + aclTwoExpectedPermissionSet + ", Found: " + aclTwoNormalUserPermissionSet, aclTwoExpectedPermissionSet, aclTwoNormalUserPermissionSet);
 
 		// Verify the set of permissions for /acl_three/role/read-only-user are exactly what we expect
 		BitSet aclThreeReadOnlyUserPermissionSet = aclThreeReadOnlyUser.getChildPermissions();
-		BitSet aclThreeExpectedPermissionSet = new BitSet(27);
-		aclThreeExpectedPermissionSet.set(22);
+		BitSet aclThreeExpectedPermissionSet = new BitSet(24);
+		aclThreeExpectedPermissionSet.set(19);
+		aclThreeExpectedPermissionSet.set(21);
 		aclThreeExpectedPermissionSet.set(24);
-		aclThreeExpectedPermissionSet.set(27);
 		assertEquals("Expected: Permissions for /acl_three/role/read-only-user = " + aclThreeExpectedPermissionSet + ", Found: " + aclThreeReadOnlyUserPermissionSet, aclThreeExpectedPermissionSet, aclThreeReadOnlyUserPermissionSet);
 
 		aclc.printErrorsAndWarnings();
     }
+
+	public void testACLPermissionProgrammaticRobustness() throws DataModelException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException, IOException
+	{
+		AccessControlListsComponent aclc =
+		        (AccessControlListsComponent) XdmComponentFactory.get(
+		                AccessControlListsComponent.class,
+		                new Resource(AccessControlListTest.class, RESOURCE_NAME_THREE),
+		                XdmComponentFactory.XDMCOMPFLAGS_DEFAULT);
+
+		// Verify _something_ was loaded...
+		assertNotNull(aclc);
+
+		// Verify exactly _three_ ACL was loaded...
+		AccessControlListsManager aclm = aclc.getItems();
+		assertNotNull("Expected: AccessControlListsManager object, Found: null", aclm);
+		AccessControlLists acls = aclm.getAccessControlLists();
+		Integer expectedNumACLs = new Integer (3);
+		assertNotNull("Expected: AccessControlLists object, Found: null", acls);
+		assertEquals("Expected: " + expectedNumACLs + " ACLs, Found: " + acls.size(), expectedNumACLs.intValue(), acls.size());
+
+		AccessControlList aclThree = null;
+		aclThree = acls.getAccessControlList("blech");
+		assertNull("Attempting to get a non-existant ACL actually returned something", aclThree);
+
+        aclThree = acls.getAccessControlList("acl_three");
+		assertNotNull("Attempting to get a valid ACL returned null", aclThree);
+
+		Permission readOnlyUser = null;
+		boolean exceptionThrown = true;
+		try {
+			readOnlyUser = aclThree.getPermission("/acl_three/role/readonlyuser");
+			exceptionThrown = false;
+		} catch (PermissionNotFoundException e) {
+			assertTrue("No exception thrown on attempt to get invalid permission", exceptionThrown);
+		}
+
+        exceptionThrown = true;
+
+		try {
+			readOnlyUser = aclThree.getPermission("/acl_three/role/read-only-user");
+			exceptionThrown = false;
+		} catch (PermissionNotFoundException e) {
+			assertFalse("Exception thrown on attempt to get _valid_ permission", exceptionThrown);
+		}
+
+		assertNotNull("Expecting a non-null Permission but found null", readOnlyUser);
+	}
+
+	public void testACLProgrammaticRobustness() throws DataModelException, InvocationTargetException, InstantiationException, NoSuchMethodException, IllegalAccessException, IOException
+	{
+		AccessControlListsComponent aclc =
+		        (AccessControlListsComponent) XdmComponentFactory.get(
+		                AccessControlListsComponent.class,
+		                new Resource(AccessControlListTest.class, RESOURCE_NAME_THREE),
+		                XdmComponentFactory.XDMCOMPFLAGS_DEFAULT);
+
+		// Verify _something_ was loaded...
+		assertNotNull(aclc);
+
+		// Verify exactly _three_ ACL was loaded...
+		AccessControlListsManager aclm = aclc.getItems();
+		assertNotNull("Expected: AccessControlListsManager object, Found: null", aclm);
+		AccessControlLists acls = aclm.getAccessControlLists();
+		Integer expectedNumACLs = new Integer (3);
+		assertNotNull("Expected: AccessControlLists object, Found: null", acls);
+		assertEquals("Expected: " + expectedNumACLs + " ACLs, Found: " + acls.size(), expectedNumACLs.intValue(), acls.size());
+
+		AccessControlList aclThree = null;
+		aclThree = acls.getAccessControlList("blech");
+		assertNull("Attempting to get a non-existant ACL actually returned something", aclThree);
+
+        aclThree = acls.getAccessControlList("acl_three");
+		assertNotNull("Attempting to get a valid ACL returned null", aclThree);
+
+		AccessControlLists aclThreeManager = aclThree.getManager();
+		assertNotNull("aclThree manager is null", aclThreeManager);
+		assertEquals("aclThreeManager != aclm", acls, aclThreeManager);
+
+	}
 
 }
