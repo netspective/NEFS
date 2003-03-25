bda82addf535c653c9c0f8a5a183490ef1823471
shahbaz.javeed <shahbaz.javeed>
2003-03-25 17:51:31 +0000

    * Added all tests to the suite

--- Commons/src/java/test/com/netspective/commons/io/Suite.java
+++ Commons/src/java/test/com/netspective/commons/io/Suite.java
@@ -4,7 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestSuite;
 
 /**
- * $Id: Suite.java,v 1.1 2003-03-25 08:03:38 shahbaz.javeed Exp $
+ * $Id: Suite.java,v 1.2 2003-03-25 17:51:31 shahbaz.javeed Exp $
  */
 public class Suite
 {
@@ -15,6 +15,7 @@ public class Suite
 		suite.addTest(new TestSuite(RegexpFileFilterTest.class));
 		suite.addTest(new TestSuite(FileTrackerTest.class));
 		suite.addTest(new TestSuite(ResourceTest.class));
+		suite.addTest(new TestSuite(InheritableFileResourcesTest.class));
 
 		return suite;
 	}
