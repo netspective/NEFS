268cab18895bfccb17c30d806dada557035a21a3
shahbaz.javeed <shahbaz.javeed>
2003-03-25 08:01:31 +0000

    * Fixed a few problems with the algorithms that match files * Fixed formatting of the code

--- Commons/src/java/main/com/netspective/commons/io/RegexpFileFilter.java
+++ Commons/src/java/main/com/netspective/commons/io/RegexpFileFilter.java
@@ -5,7 +5,7 @@ import org.apache.oro.text.perl.Perl5Util;
 import org.apache.oro.text.regex.*;
 
 /**
- * $Id: RegexpFileFilter.java,v 1.1 2003-03-23 16:42:41 shahbaz.javeed Exp $
+ * $Id: RegexpFileFilter.java,v 1.2 2003-03-25 08:01:31 shahbaz.javeed Exp $
  */
 public class RegexpFileFilter implements java.io.FileFilter
 {
@@ -39,16 +39,21 @@ public class RegexpFileFilter implements java.io.FileFilter
 	 *          should be included
 	 */
 	public boolean accept (File pathname) {
+		// if we're given a null pattern, return false...
+		if (null == fileRegexp)
+			return false;
+
 		// if the pattern hasnt been initialized yet, initialize it...
-		if (!patternInitialized && null == pattern)
+		if (!patternInitialized)
 			try {
-				pattern = pc.compile(fileRegexp);
+				pattern = pc.compile(fileRegexp, patternType);
+				patternInitialized = true;
 			} catch (MalformedPatternException e) {
 				pattern = null;
 				patternInitialized = true;
 			}
 
-		// if the pattern is null => previous initialization failed => return false
+		// if the pattern is null => previous initialization failed => return false so no files are 'accepted'
 		if (null == pattern)
 			return false;
 
@@ -57,33 +62,43 @@ public class RegexpFileFilter implements java.io.FileFilter
 			return (allowDirectories ? true : false);
 
 		// otherwise, do a regexp match...
-		return pm.matches(pathname.getName(), pattern);
+		return pm.contains(pathname.getName(), pattern);
 	}
 
-	public String getFileRegexp () {
+	public String getFileRegexp ()
+	{
 		return fileRegexp;
 	}
 
-	public void setFileRegexp (String fileRegexp) {
+	public void setFileRegexp (String fileRegexp)
+	{
 		this.fileRegexp = fileRegexp;
 		patternInitialized = false;
 	}
 
     public void setCaseSensitive(boolean sensitivity)
     {
-	    patternType = Perl5Compiler.DEFAULT_MASK | Perl5Compiler.SINGLELINE_MASK | Perl5Compiler.CASE_INSENSITIVE_MASK;
+	    if (false == sensitivity)
+	        patternType = Perl5Compiler.DEFAULT_MASK | Perl5Compiler.SINGLELINE_MASK | Perl5Compiler.CASE_INSENSITIVE_MASK;
+	    else
+	        patternType = Perl5Compiler.DEFAULT_MASK;
+
+	    patternInitialized = false;
     }
 
 	public boolean isCaseSensitive()
 	{
-		return ((patternType & Perl5Compiler.CASE_INSENSITIVE_MASK) == Perl5Compiler.CASE_INSENSITIVE_MASK);
+		return ((patternType & Perl5Compiler.CASE_INSENSITIVE_MASK) == 0);
 	}
 
-	public boolean isAllowDirectories () {
+	public boolean isAllowDirectories ()
+	{
 		return allowDirectories;
 	}
 
-	public void setAllowDirectories (boolean allowDirectories) {
+	public void setAllowDirectories (boolean allowDirectories)
+	{
 		this.allowDirectories = allowDirectories;
+		patternInitialized = false;
 	}
 }
