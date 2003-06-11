152455d33d1ffa857c446d4532e7d5ac23a2fb78
roque.hernandez <roque.hernandez>
2003-06-11 23:23:55 +0000

    Started laying the foundation for the schema Unit Tests.  No assertions yet, simply setting up the stage.

---
+++ Axiom/src/java/test/com/netspective/axiom/schema/SchemaColumnsTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * @author Shahid N. Shah
+ */
+
+/**
+ * $Id: SchemaColumnsTest.java,v 1.1 2003-06-11 23:23:55 roque.hernandez Exp $
+ */
+
+package com.netspective.axiom.schema;
+
+import java.sql.SQLException;
+import java.sql.Types;
+import java.sql.ResultSet;
+import java.util.Set;
+import java.util.Map;
+
+import javax.naming.NamingException;
+
+import junit.framework.TestCase;
+
+import com.netspective.commons.xdm.XdmComponentFactory;
+import com.netspective.commons.xdm.exception.DataModelException;
+import com.netspective.commons.io.Resource;
+import com.netspective.commons.text.TextUtils;
+import com.netspective.commons.value.ValueSources;
+import com.netspective.axiom.sql.*;
+import com.netspective.axiom.sql.collection.QueriesCollection;
+import com.netspective.axiom.sql.collection.QueriesPackage;
+import com.netspective.axiom.*;
+import com.netspective.axiom.connection.DriverManagerConnectionProvider;
+import com.netspective.axiom.policy.OracleDatabasePolicy;
+import com.netspective.axiom.policy.PostgreSqlDatabasePolicy;
+import com.netspective.axiom.value.DatabaseConnValueContext;
+import com.netspective.axiom.value.BasicDatabaseConnValueContext;
+import com.netspective.axiom.value.BasicDatabasePolicyValueContext;
+
+public class SchemaColumnsTest extends TestCase
+{
+    public static final String RESOURCE_NAME = "SqlManagerQueryTest.xml";
+	 protected SqlManagerComponent component = null;
+	 protected SqlManager manager = null;
+    protected String[] queryNames = new String[] { "statement-0", "statement-1", "bad-statement", "statement-2" };
+    protected String[] fqQueryNames = new String[]{"test.statement-0", "test.statement-1", "test.bad-statement", "statement-2"};
+
+	protected void setUp () throws Exception
+    {
+		super.setUp();
+	}
+
+   public void test1(){
+       TestUtils.getConnProvider(this.getClass().getPackage().getName(), false, false);
+   }
+
+}
---
+++ Axiom/src/java/test/com/netspective/axiom/schema/Suite.java
@@ -0,0 +1,20 @@
+package com.netspective.axiom.schema;
+
+import junit.framework.TestSuite;
+import junit.framework.Test;
+import com.netspective.axiom.sql.MiscSqlObjectsTest;
+import com.netspective.axiom.sql.SqlManagerQueryTest;
+import com.netspective.axiom.sql.DynamicSqlTest;
+
+/**
+ * $Id: Suite.java,v 1.1 2003-06-11 23:23:55 roque.hernandez Exp $
+ */
+public class Suite extends TestSuite
+{
+    public static Test suite()
+    {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new TestSuite(SchemaColumnsTest.class));
+        return suite;
+    }
+}
