fc9b1bfda2d9537d41d10e7429956850ce973084
shahid.shah <shahid.shah>
2003-06-13 02:04:20 +0000

    Added javadoc-html and java-xml targets. The javadoc-html will create HTML JavaDocs in the documents/java-doc folder. The javadoc-xml target will create XML files containing the JavaDoc information and place those into the src/java/main/conf/java-doc directory so that the information is available at runtime (mainly for Console in Sparx).

---
+++ Sparx/conf/build.bat
@@ -0,0 +1,23 @@
+@echo off
+
+REM $Id: build.bat,v 1.1 2003-06-13 02:04:20 shahid.shah Exp $
+
+REM **************************************************************************
+REM ** This script should be be run from the PROJECT_HOME\conf directory.   **
+REM ** It is basically a "launcher" for Ant and the actual work is done in  **
+REM ** the build.xml file.                                                  **
+REM **************************************************************************
+
+if "%JAVA_HOME%" == "" echo Error: JAVA_HOME environment variable is not set. && goto end
+if "%NS_COMMONS_HOME%" == "" set NS_COMMONS_HOME=..\..\Commons
+if "%NS_COMMONS_REDIST_LIB%" == "" set NS_COMMONS_REDIST_LIB=%NS_COMMONS_HOME%\lib\redist
+
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java
+if not exist "%JAVACMD%.exe" echo Error: "%JAVACMD%.exe" not found - check JAVA_HOME && goto end
+
+if exist "%JAVA_HOME%/lib/tools.jar" set JAVACP=%JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%/lib/classes.zip" set JAVACP=%JAVACP%;%JAVA_HOME%\lib\classes.zip
+
+%JAVACMD% -classpath %NS_COMMONS_REDIST_LIB%\ant.jar;%NS_COMMONS_REDIST_LIB%\ant-optional.jar;%NS_COMMONS_REDIST_LIB%\junit.jar;%NS_COMMONS_REDIST_LIB%\clover.jar;%NS_COMMONS_REDIST_LIB%\xerces.jar;%JAVACP% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:end
