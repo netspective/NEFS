0a522fe8c68c209b1e58613d996ed23e8a6c1a4f
shahid.shah <shahid.shah>
2003-06-14 22:39:50 +0000

    no message

---
+++ Sparx/src/java/main/com/netspective/sparx/BuildLog.java
@@ -0,0 +1,27 @@
+package com.netspective.sparx;
+
+public class BuildLog
+{
+  public static final int    BUILD_NUMBER = 7;
+  public static final String BUILD_HOST_NAME = "moscow";
+  public static final String BUILD_HOST_IP = "192.168.0.103";
+  public static final String BUILD_DATE = "Sat Jun 14 16:49:24 EDT 2003";
+
+  public static final String BUILD_OS_NAME = "Windows 2000";
+  public static final String BUILD_OS_VERSION = "5.1";
+
+  public static final String BUILD_JAVA_VERSION = "1.3.1_02";
+  public static final String BUILD_JAVA_VENDOR = "Sun Microsystems Inc.";
+
+  public static final String BUILD_VM_NAME = "Java HotSpot(TM) Client VM";
+  public static final String BUILD_VM_VERSION = "1.3.1_02-b02";
+  public static final String BUILD_VM_VENDOR = "Sun Microsystems Inc.";
+
+  public static final String[] BUILD_CLASS_PATH = new String[] {
+      "C:/Projects/Frameworks/Sparx/conf/../../Commons/lib/redist/ant.jar", 
+      "C:/Projects/Frameworks/Sparx/conf/../../Commons/lib/redist/ant-optional.jar", 
+      "C:/Projects/Frameworks/Sparx/conf/../../Commons/lib/redist/junit.jar", 
+      "C:/Projects/Frameworks/Sparx/conf/../../Commons/lib/redist/clover.jar", 
+      "C:/Projects/Frameworks/Sparx/conf/../../Commons/lib/redist/xerces.jar", 
+      "C:/utils/java/jdk1.3.1/lib/tools.jar"      };
+}
