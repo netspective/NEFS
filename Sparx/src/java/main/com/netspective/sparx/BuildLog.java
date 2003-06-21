bd246d7f3188ba9142a30562dcfd1679ce1f2533
shahid.shah <shahid.shah>
2003-06-21 23:41:32 +0000

    Added new Console page called '/console/project/build' and associated dialog that allows running Ant built files from within Console. Now the Sparx application build file can be run from within the Console.

--- Sparx/src/java/main/com/netspective/sparx/BuildLog.java
+++ Sparx/src/java/main/com/netspective/sparx/BuildLog.java
@@ -2,10 +2,10 @@ package com.netspective.sparx;
 
 public class BuildLog
 {
-  public static final int    BUILD_NUMBER = 9;
+  public static final int    BUILD_NUMBER = 10;
   public static final String BUILD_HOST_NAME = "moscow";
-  public static final String BUILD_HOST_IP = "192.168.0.103";
-  public static final String BUILD_DATE = "Sun Jun 15 14:17:21 EDT 2003";
+  public static final String BUILD_HOST_IP = "192.168.0.101";
+  public static final String BUILD_DATE = "Sat Jun 21 18:24:22 EDT 2003";
 
   public static final String BUILD_OS_NAME = "Windows 2000";
   public static final String BUILD_OS_VERSION = "5.1";
---
+++ Sparx/src/java/main/com/netspective/sparx/console/form/AntBuildDialog.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * @author Shahid N. Shah
+ */
+
+/**
+ * $Id: AntBuildDialog.java,v 1.1 2003-06-21 23:41:32 shahid.shah Exp $
+ */
+
+package com.netspective.sparx.console.form;
+
+import java.io.Writer;
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.BuildLogger;
+import org.apache.tools.ant.NoBannerLogger;
+
+import com.netspective.sparx.console.form.ConsoleDialog;
+import com.netspective.sparx.form.DialogContext;
+import com.netspective.sparx.form.DialogExecuteException;
+import com.netspective.commons.value.Value;
+
+public class AntBuildDialog extends ConsoleDialog
+{
+    private static final Log log = LogFactory.getLog(AntBuildDialog.class);
+
+    public void execute(Writer writer, DialogContext dc) throws IOException, DialogExecuteException
+    {
+        DialogContext.DialogFieldStates states = dc.getFieldStates();
+
+        Value projectValue = states.getState("project").getValue();
+        Value targetValue = states.getState("target").getValue();
+        File projectFile = new File(projectValue.getTextValue());
+
+        Project project = new Project();
+        project.init();
+
+        File buildFile = new File(projectFile.getParentFile(), projectFile.getName());
+        ProjectHelper.configureProject(project, buildFile);
+
+        ByteArrayOutputStream ostream = new ByteArrayOutputStream();
+        PrintStream pstream = new PrintStream(ostream);
+
+        BuildLogger logger = new NoBannerLogger();
+        logger.setMessageOutputLevel(Project.MSG_INFO);
+        logger.setOutputPrintStream(pstream);
+        logger.setErrorPrintStream(pstream);
+
+        project.addBuildListener(logger);
+        project.executeTarget(targetValue.getTextValue());
+
+        writer.write("<div class='textbox'><b>Ant Output</b><p><pre>");
+        writer.write(ostream.toString());
+        writer.write("</pre>");
+    }
+}
