464593418b70bb685af38cbca5987d312349840f
aye.thu <aye.thu>
2003-07-31 00:24:55 +0000

    A specialized dialog context for the Query Dialog. This context keeps track of report related items such as rows per page, report skin, and report name.

---
+++ Sparx/src/java/main/com/netspective/sparx/form/sql/QueryDialogContext.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * @author Shahid N. Shah
+ */
+package com.netspective.sparx.form.sql;
+
+import com.netspective.sparx.form.DialogContext;
+import com.netspective.sparx.panel.QueryReportPanel;
+import com.netspective.sparx.sql.Query;
+
+import java.io.IOException;
+
+/**
+ * @author aye
+ * $Id: QueryDialogContext.java,v 1.1 2003-07-31 00:24:55 aye.thu Exp $
+ */
+public class QueryDialogContext extends DialogContext
+{
+    private int rowsPerPage = 0;
+    private String reportPanelName;
+    private String reportSkinName;
+    private String queryName;
+    private QueryReportPanel reportPanel;
+
+    public String getQueryName()
+    {
+        return queryName;
+    }
+
+    public void setQueryName(String queryName)
+    {
+        this.queryName = queryName;
+    }
+
+    public String getReportSkinName()
+    {
+        return reportSkinName;
+    }
+
+    public void setReportSkinName(String reportSkinName)
+    {
+        this.reportSkinName = reportSkinName;
+    }
+
+    public String getReportPanelName()
+    {
+        return reportPanelName;
+    }
+
+    public void setReportPanelName(String reportPanel)
+    {
+        this.reportPanelName = reportPanel;
+    }
+
+    public int getRowsPerPage()
+    {
+        return rowsPerPage;
+    }
+
+    public void setRowsPerPage(int rowsPerPage)
+    {
+        this.rowsPerPage = rowsPerPage;
+    }
+
+    public String getStateHiddens()
+    {
+        QueryDialog qDialog = (QueryDialog) getDialog();
+        StringBuffer sb = new StringBuffer();
+        if (rowsPerPage > 0)
+            sb.append("<input type='hidden' name='" + qDialog.getRowsPerPageParamName() + "' value='" + rowsPerPage + "'>\n");
+
+        if (reportPanelName != null)
+            sb.append("<input type='hidden' name='" + qDialog.getReportPanelParamName() + "' value='" + reportPanelName + "'>\n");
+        if (reportSkinName != null)
+            sb.append("<input type='hidden' name='" + qDialog.getReportPanelParamName() + "' value='" + reportSkinName + "'>\n");
+        if (queryName != null)
+            sb.append("<input type='hidden' name='" + qDialog.getReportPanelParamName() + "' value='" + queryName + "'>\n");
+        return (super.getStateHiddens() + sb.toString());
+    }
+
+    /**
+     * Gets the report panel for the current context
+     * @return
+     */
+    public QueryReportPanel getReportPanel()
+    {
+        if (reportPanel == null)
+        {
+            reportPanel = getQueryReportPanel();
+        }
+        return reportPanel;
+    }
+
+    /**
+     * Gets the query report panel from the dialog's query object based on the report name
+     * @return
+     */
+    private QueryReportPanel getQueryReportPanel()
+    {
+        QueryReportPanel result = null;
+        Query query = ((QueryDialog)getDialog()).getQuery();
+
+        if (reportPanelName != null)
+            result = query.getPresentation().getPanel(reportPanelName);
+
+        if (result == null)
+            result = query.getPresentation().getDefaultPanel();
+
+        if (reportSkinName != null)
+            result.setReportSkin(reportSkinName);
+        if (rowsPerPage > 0)
+        {
+            result.setScrollRowsPerPage(rowsPerPage);
+            result.setScrollable(true);
+        }
+
+        return result;
+    }
+}
