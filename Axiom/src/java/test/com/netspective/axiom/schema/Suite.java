c5b9630346cbcc1616dfdf8d421079813d561841
roque.hernandez <roque.hernandez>
2003-09-17 12:58:13 +0000

    Changed the order in which the test cases were run.  One of the test cases is modifying the data, thus the test cases after that fail.  The data should be reloaded everytime with a fresh data set depending on the conf files.  This is related to bug 98 but the bug won't be close until we figure out why the data is not being re-initialized.  But this allows the tests to run succesfully.

--- Axiom/src/java/test/com/netspective/axiom/schema/Suite.java
+++ Axiom/src/java/test/com/netspective/axiom/schema/Suite.java
@@ -7,16 +7,17 @@ import com.netspective.axiom.sql.SqlManagerQueryTest;
 import com.netspective.axiom.sql.DynamicSqlTest;
 
 /**
- * $Id: Suite.java,v 1.3 2003-06-30 04:27:15 roque.hernandez Exp $
+ * $Id: Suite.java,v 1.4 2003-09-17 12:58:13 roque.hernandez Exp $
  */
 public class Suite extends TestSuite
 {
     public static Test suite()
     {
         TestSuite suite = new TestSuite();
-        suite.addTest(new TestSuite(SchemaColumnsTest.class));
-        suite.addTest(new TestSuite(SchemaTableTest.class));
         suite.addTest(new TestSuite(SchemaConstraintTest.class));
+        suite.addTest(new TestSuite(SchemaTableTest.class));
+        suite.addTest(new TestSuite(SchemaColumnsTest.class));
+
         return suite;
     }
 }
