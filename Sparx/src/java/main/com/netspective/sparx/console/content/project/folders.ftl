8682cebc292ff339d567cc83bf86e20a82db374d
shahid.shah <shahid.shah>
2003-10-09 15:11:52 +0000

    Additional features to the Console/Project/Folders page

--- Sparx/src/java/main/com/netspective/sparx/console/content/project/folders.ftl
+++ Sparx/src/java/main/com/netspective/sparx/console/content/project/folders.ftl
@@ -1,21 +1,43 @@
 <#include "*/library.ftl"/>
 
-<#macro showSelfDescendants activePath classSuffix>
-    <tr><td class="report-column-${classSuffix}">
+<#macro showSelfDescendants activePath classSuffix fileTypesToCount>
+    <#assign children = activePath.children/>
+
+    <tr>
+    <td class="report-column-${classSuffix}">
         <nobr>
-        <#list 0..activePath.level as i>&nbsp;&nbsp;&nbsp;&nbsp;</#list>
+        <#if activePath.level gt 0>
+        <#list 1..activePath.level as i>&nbsp;&nbsp;&nbsp;&nbsp;</#list>
+        </#if>
         <img src="${vc.activeTheme.getResourceUrl('/images/files/file-type-folder-closed.gif')}"/>
         ${vc.getConsoleFileBrowserLinkShowAlt(activePath.file.absolutePath, activePath.file.name)}
         </nobr>
-    </td></tr>
+    </td>
+    <#assign fileCounts = {}/>
+    <#assign dirList = activePath.file.list()/>
+    <#list children as child>
+        <#assign fileType = child.entryType/>
+        <#assign fileCounts = fileCounts + { fileType : fileCounts[fileType]?default(0) + 1 } />
+    </#list>
 
-    <#list activePath.children as child>
+    <td class="report-column-${classSuffix}" align=right>${children?size}</td>
+
+    <#list fileTypesToCount as fileType>
+        <#if fileCounts[fileType]?exists>
+            <td class="report-column-${classSuffix}" align=right>${fileCounts[fileType]}</td>
+        <#else>
+            <td class="report-column-${classSuffix}" align=right>&nbsp;</td>
+        </#if>
+    </#list>
+    </tr>
+
+    <#list children as child>
         <#assign include = true/>
         <#if child.file.name = 'CVS'><#assign include=false/></#if>
 
         <#if include && child.file.isDirectory()>
             <#if classSuffix = 'odd'><#assign nextClassSuffix='even'/><#else><#assign nextClassSuffix='odd'/></#if>
-            <@showSelfDescendants activePath=child classSuffix=nextClassSuffix/>
+            <@showSelfDescendants activePath=child classSuffix=nextClassSuffix fileTypesToCount=fileTypesToCount/>
         </#if>
     </#list>
 </#macro>
@@ -25,9 +47,18 @@
     <#assign projectFilesContext = vc.navigationContext.projectFileSystemContext/>
     <#assign activePath = projectFilesContext.activePath/>
 
+    <#assign allFileTypes = activePath.getFileTypes(true)/>
+
     <#if projectFilesContext.activePath.file.isDirectory()>
         <table cellspacing=5>
-            <@showSelfDescendants activePath=projectFilesContext.activePath classSuffix='odd'/>
+            <tr>
+                <td class="report-column-heading">Folder</td>
+                <td class="report-column-heading">Files</td>
+                <#list allFileTypes as fileType>
+                    <td class="report-column-heading">*.${fileType}</td>
+                </#list>
+            </tr>
+            <@showSelfDescendants activePath=projectFilesContext.activePath classSuffix='odd' fileTypesToCount=allFileTypes/>
         </table>
     </#if>
 
--- Sparx/src/java/main/com/netspective/sparx/navigate/FileSystemEntry.java
+++ Sparx/src/java/main/com/netspective/sparx/navigate/FileSystemEntry.java
@@ -3,55 +3,55 @@
  *
  * Netspective Corporation permits redistribution, modification and use
  * of this file in source and binary form ("The Software") under the
- * Netspective Source License ("NSL" or "The License"). The following 
- * conditions are provided as a summary of the NSL but the NSL remains the 
+ * Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the
  * canonical license and must be accepted before using The Software. Any use of
- * The Software indicates agreement with the NSL. 
+ * The Software indicates agreement with the NSL.
  *
  * 1. Each copy or derived work of The Software must preserve the copyright
  *    notice and this notice unmodified.
  *
- * 2. Redistribution of The Software is allowed in object code form only 
- *    (as Java .class files or a .jar file containing the .class files) and only 
- *    as part of an application that uses The Software as part of its primary 
- *    functionality. No distribution of the package is allowed as part of a software 
- *    development kit, other library, or development tool without written consent of 
- *    Netspective Corporation. Any modified form of The Software is bound by 
+ * 2. Redistribution of The Software is allowed in object code form only
+ *    (as Java .class files or a .jar file containing the .class files) and only
+ *    as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software
+ *    development kit, other library, or development tool without written consent of
+ *    Netspective Corporation. Any modified form of The Software is bound by
  *    these same restrictions.
- * 
- * 3. Redistributions of The Software in any form must include an unmodified copy of 
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of
  *    The License, normally in a plain ASCII text file unless otherwise agreed to,
  *    in writing, by Netspective Corporation.
  *
- * 4. The names "Sparx" and "Netspective" are trademarks of Netspective 
- *    Corporation and may not be used to endorse products derived from The 
- *    Software without without written consent of Netspective Corporation. "Sparx" 
- *    and "Netspective" may not appear in the names of products derived from The 
+ * 4. The names "Sparx" and "Netspective" are trademarks of Netspective
+ *    Corporation and may not be used to endorse products derived from The
+ *    Software without without written consent of Netspective Corporation. "Sparx"
+ *    and "Netspective" may not appear in the names of products derived from The
  *    Software without written consent of Netspective Corporation.
  *
- * 5. Please attribute functionality to Sparx where possible. We suggest using the 
+ * 5. Please attribute functionality to Sparx where possible. We suggest using the
  *    "powered by Sparx" button or creating a "powered by Sparx(tm)" link to
  *    http://www.netspective.com for each application using Sparx.
  *
- * The Software is provided "AS IS," without a warranty of any kind. 
+ * The Software is provided "AS IS," without a warranty of any kind.
  * ALL EXPRESS OR IMPLIED REPRESENTATIONS AND WARRANTIES, INCLUDING ANY
  * IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
  * OR NON-INFRINGEMENT, ARE HEREBY DISCLAIMED.
  *
  * NETSPECTIVE CORPORATION AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES
- * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING 
+ * SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A RESULT OF USING OR DISTRIBUTING
  * THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE
  * FOR ANY LOST REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL,
  * CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER CAUSED AND
  * REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR
  * INABILITY TO USE THE SOFTWARE, EVEN IF HE HAS BEEN ADVISED OF THE POSSIBILITY
- * OF SUCH DAMAGES.      
+ * OF SUCH DAMAGES.
  *
  * @author Shahid N. Shah
  */
- 
+
 /**
- * $Id: FileSystemEntry.java,v 1.2 2003-10-08 21:14:26 shahid.shah Exp $
+ * $Id: FileSystemEntry.java,v 1.3 2003-10-09 15:11:52 shahid.shah Exp $
  */
 
 package com.netspective.sparx.navigate;
@@ -63,6 +63,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -178,6 +180,34 @@ public class FileSystemEntry
         }
 
         return result;
+    }
+
+    public static void getDirectoryFileTypes(Set types, File dir, boolean recursive)
+    {
+        if(! dir.isDirectory())
+            return;
+
+        File[] children = dir.listFiles();
+        if(children == null)
+            return;
+
+        for(int i = 0; i < children.length; i++)
+        {
+            File child = children[i];
+            int extnIndex = child.getName().lastIndexOf('.');
+            if(extnIndex > -1)
+                types.add(child.getName().substring(extnIndex + 1));
+
+            if(recursive)
+                getDirectoryFileTypes(types, child, recursive);
+        }
+    }
+
+    public String[] getFileTypes(boolean recursive)
+    {
+        Set result = new TreeSet();
+        getDirectoryFileTypes(result, file, recursive);
+        return (String[]) result.toArray(new String[result.size()]);
     }
 
     public ArrayList getChildren()
