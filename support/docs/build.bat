2a7630aa3ea275412afac7573cc9d2c601d6735e
shahid.shah <shahid.shah>
2003-10-27 19:23:27 +0000

    Switched from Saxon to TrAX for stylesheets.

--- support/docs/build.bat
+++ support/docs/build.bat
@@ -1,17 +1,24 @@
 @echo off
 
-REM $Id: build.bat,v 1.1 2003-10-27 18:36:18 shahid.shah Exp $
+REM $Id: build.bat,v 1.2 2003-10-27 19:23:27 shahid.shah Exp $
 
 REM **************************************************************************
-REM ** This script should be be run from the project build.xml directory.   **
+REM ** This script should be be run from the PROJECT_HOME\conf directory.   **
 REM ** It is basically a "launcher" for Ant and the actual work is done in  **
 REM ** the build.xml file.                                                  **
 REM **************************************************************************
 
+if "%JAVA_HOME%" == "" echo Error: JAVA_HOME environment variable is not set. && goto end
 if "%NEFS_HOME%" == "" set NEFS_HOME=..\..
 if "%NEFS_COMMONS_HOME%" == "" set NEFS_COMMONS_HOME=%NEFS_HOME%\Commons
 if "%NEFS_COMMONS_REDIST_LIB%" == "" set NEFS_COMMONS_REDIST_LIB=%NEFS_COMMONS_HOME%\lib\redist
 
-java -classpath %NEFS_COMMONS_REDIST_LIB%\ant.jar;%NEFS_COMMONS_REDIST_LIB%\ant-optional.jar;%NEFS_COMMONS_REDIST_LIB%\xerces.jar;%JAVACP% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+if "%JAVACMD%" == "" set JAVACMD=%JAVA_HOME%\bin\java
+if not exist "%JAVACMD%.exe" echo Error: "%JAVACMD%.exe" not found - check JAVA_HOME && goto end
+
+if exist "%JAVA_HOME%/lib/tools.jar" set JAVACP=%JAVA_HOME%\lib\tools.jar
+if exist "%JAVA_HOME%/lib/classes.zip" set JAVACP=%JAVACP%;%JAVA_HOME%\lib\classes.zip
+
+%JAVACMD% -classpath %NEFS_COMMONS_REDIST_LIB%\ant.jar;%NEFS_COMMONS_REDIST_LIB%\ant-optional.jar;%NEFS_COMMONS_REDIST_LIB%\junit.jar;%NEFS_COMMONS_REDIST_LIB%\clover.jar;%NEFS_COMMONS_REDIST_LIB%\xerces.jar;%JAVACP% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :end
