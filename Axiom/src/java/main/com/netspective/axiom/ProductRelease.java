e476f640953245aa2bfe0f15e7f1b4e8de3d0758
shahid.shah <shahid.shah>
2003-11-19 02:19:28 +0000

    Added check to see if column is required by app or dbms in order to generate the NOT NULL constraint.

--- Axiom/src/java/main/com/netspective/axiom/ProductRelease.java
+++ Axiom/src/java/main/com/netspective/axiom/ProductRelease.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: ProductRelease.java,v 1.6 2003-11-18 11:33:55 shahid.shah Exp $
+ * $Id: ProductRelease.java,v 1.7 2003-11-19 02:19:28 shahid.shah Exp $
  */
 
 package com.netspective.axiom;
@@ -56,7 +56,7 @@ public class ProductRelease implements Product
 
     public static final int PRODUCT_RELEASE_NUMBER = 7;
     public static final int PRODUCT_VERSION_MAJOR = 0;
-    public static final int PRODUCT_VERSION_MINOR = 2;
+    public static final int PRODUCT_VERSION_MINOR = 3;
 
     public ProductRelease()
     {
--- Axiom/src/java/main/com/netspective/axiom/policy/AnsiDatabasePolicy.java
+++ Axiom/src/java/main/com/netspective/axiom/policy/AnsiDatabasePolicy.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: AnsiDatabasePolicy.java,v 1.8 2003-11-16 15:15:59 shahid.shah Exp $
+ * $Id: AnsiDatabasePolicy.java,v 1.9 2003-11-19 02:17:19 shahid.shah Exp $
  */
 
 package com.netspective.axiom.policy;
@@ -1368,7 +1368,7 @@ public class AnsiDatabasePolicy implements DatabasePolicy
         if(column.isPrimaryKey())
             writer.write(" PRIMARY KEY");
 
-        if(column.isRequiredByDbms())
+        if(column.isRequiredByApp() || column.isRequiredByDbms())
             writer.write(" NOT NULL");
     }
 }
