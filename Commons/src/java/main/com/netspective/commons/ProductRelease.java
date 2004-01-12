cabc7e7a78660ce8461095b4028163f07fdae7e0
shahid.shah <shahid.shah>
2004-01-12 19:51:24 +0000

    Updated version number to publish the ConcatenateValueSource bug fix.

--- Commons/src/java/main/com/netspective/commons/ProductRelease.java
+++ Commons/src/java/main/com/netspective/commons/ProductRelease.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: ProductRelease.java,v 1.9 2003-12-10 21:00:59 shahid.shah Exp $
+ * $Id: ProductRelease.java,v 1.10 2004-01-12 19:51:24 shahid.shah Exp $
  */
 
 package com.netspective.commons;
@@ -53,7 +53,7 @@ public class ProductRelease implements Product
 
     public static final int PRODUCT_RELEASE_NUMBER = 7;
     public static final int PRODUCT_VERSION_MAJOR = 0;
-    public static final int PRODUCT_VERSION_MINOR = 5;
+    public static final int PRODUCT_VERSION_MINOR = 6;
 
     public ProductRelease()
     {
