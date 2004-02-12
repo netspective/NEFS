729dc38066f14202c0d459c0164f75def6559988
shahid.shah <shahid.shah>
2004-02-12 14:01:45 +0000

    Added new conditional field sample with auto submits on select of one field and dynamically populates the value of a partner field.

---
+++ webapps/nefs-sampler/WEB-INF/classes/app/form/input/SelectFieldsAutoSubmit.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) 2000-2003 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse products derived from The Software without without written consent of Netspective. "Netspective",
+ *    "Axiom", "Commons", "Junxion", and "Sparx" may not appear in the names of products derived from The Software
+ *    without written consent of Netspective.
+ *
+ * 5. Please attribute functionality where possible. We suggest using the "powered by Netspective" button or creating
+ *    a "powered by Netspective(tm)" link to http://www.netspective.com for each application using The Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF HE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ *
+ * @author Shahid N. Shah
+ */
+
+/**
+ * $Id: SelectFieldsAutoSubmit.java,v 1.1 2004-02-12 14:01:45 shahid.shah Exp $
+ */
+
+package app.form.input;
+
+import com.netspective.sparx.form.Dialog;
+import com.netspective.sparx.form.DialogsPackage;
+import com.netspective.sparx.form.DialogContext;
+import com.netspective.sparx.form.field.DialogFieldStates;
+import com.netspective.sparx.form.field.DialogFieldValue;
+import com.netspective.sparx.form.field.type.SelectField;
+import com.netspective.sparx.Project;
+import com.netspective.commons.value.source.AbstractValueSource;
+import com.netspective.commons.value.ValueContext;
+import com.netspective.commons.value.Value;
+import com.netspective.commons.value.PresentationValue;
+
+public class SelectFieldsAutoSubmit extends Dialog
+{
+    public SelectFieldsAutoSubmit(Project project)
+    {
+        super(project);
+    }
+
+    public SelectFieldsAutoSubmit(Project project, DialogsPackage pkg)
+    {
+        super(project, pkg);
+    }
+
+    public void finalizeContents()
+    {
+        super.finalizeContents();
+
+        // we are creating a dynamic value source so that the partner can be filled in using the selected value of the
+        // main field (if this was a "normal" value source we would just assign it in the XML 'choices' attribute)
+        SelectField related = (SelectField) getFields().getByName("sel_field_related");
+        related.setChoices(new PartnerValueSource());
+    }
+
+    public boolean isValid(DialogContext dc)
+    {
+        if(! super.isValid(dc))
+            return false;
+
+        // if our related field is not populated yet, be sure set the dialog as invalid so that we don't execute yet
+        DialogFieldStates dfs = dc.getFieldStates();
+        if(! dfs.getState("sel_field_related").hasRequiredValue())
+            return false;
+
+        // if we have a value in the partner then we're ok.
+        return true;
+    }
+
+    protected class PartnerValueSource extends AbstractValueSource
+    {
+        public PartnerValueSource()
+        {
+            super();
+        }
+
+        public boolean hasValue(ValueContext vc)
+        {
+            return true;
+        }
+
+        public Value getValue(ValueContext vc)
+        {
+            return getPresentationValue(vc);
+        }
+
+        public PresentationValue getPresentationValue(ValueContext vc)
+        {
+            DialogContext dc = (DialogContext) vc;
+            DialogFieldStates dfs = dc.getFieldStates();
+            DialogFieldValue mainValue = dfs.getState("sel_field_main").getValue();
+
+            PresentationValue result = new PresentationValue();
+            if(mainValue.hasValue())
+            {
+                String mainTextValue = mainValue.getTextValue();
+                PresentationValue.Items items = result.createItems();
+                for(int i = 0; i < 5; i++)
+                    items.addItem("Choice " + mainTextValue + " " + i, mainTextValue + i);
+            }
+
+            return result;
+        }
+    }
+
+}
