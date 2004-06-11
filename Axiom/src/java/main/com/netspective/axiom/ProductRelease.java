11fffce87ba76654849039d7d36836a1e270399d
shahid.shah <shahid.shah>
2004-06-11 02:24:43 +0000

    Added experimental support for quoting SQL columns in Query Definitions so that auto-generated table query defns are allowed to have numeric and other "wierd" column names without causing a SQL exception. Two new attributes have been introduced:
    <table name="x" quote-name-in-sql="yes">
      <column name="y" quote-name-in-sql="yes">
    
    If the quote-name-in-sql attribute is set to yes, then the columns will be quoted when generating SQL. At this time, only the SQL generated by query defns is quoted appropriately but after testing we will want to add it to DDL generation and insert/update/delete SQL generation, too.

--- Axiom/src/java/main/com/netspective/axiom/ProductRelease.java
+++ Axiom/src/java/main/com/netspective/axiom/ProductRelease.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: ProductRelease.java,v 1.20 2004-05-19 22:04:18 aye.thu Exp $
+ * $Id: ProductRelease.java,v 1.21 2004-06-11 02:24:43 shahid.shah Exp $
  */
 
 package com.netspective.axiom;
@@ -55,7 +55,7 @@ public class ProductRelease implements Product
 
     public static final int PRODUCT_RELEASE_NUMBER = 7;
     public static final int PRODUCT_VERSION_MAJOR = 1;
-    public static final int PRODUCT_VERSION_MINOR = 2;
+    public static final int PRODUCT_VERSION_MINOR = 3;
 
     public ProductRelease()
     {
