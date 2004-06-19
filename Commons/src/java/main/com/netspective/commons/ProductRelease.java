1184feccebde2959d43ed9bebddc1bdac08ca18d
shahid.shah <shahid.shah>
2004-06-19 01:30:57 +0000

    Bumping version to 7.1.2.

--- Commons/src/java/main/com/netspective/commons/ProductRelease.java
+++ Commons/src/java/main/com/netspective/commons/ProductRelease.java
@@ -39,7 +39,7 @@
  */
 
 /**
- * $Id: ProductRelease.java,v 1.14 2004-03-24 01:18:59 shahid.shah Exp $
+ * $Id: ProductRelease.java,v 1.15 2004-06-19 01:30:57 shahid.shah Exp $
  */
 
 package com.netspective.commons;
@@ -53,7 +53,7 @@ public class ProductRelease implements Product
 
     public static final int PRODUCT_RELEASE_NUMBER = 7;
     public static final int PRODUCT_VERSION_MAJOR = 1;
-    public static final int PRODUCT_VERSION_MINOR = 1;
+    public static final int PRODUCT_VERSION_MINOR = 2;
 
     public ProductRelease()
     {
