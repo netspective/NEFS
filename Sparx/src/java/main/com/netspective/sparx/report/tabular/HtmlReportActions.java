4e51847d3a28b85553fb479f4a11ee001ff8faf2
aye.thu <aye.thu>
2004-06-23 15:15:52 +0000

    Removed saving report actions in a map since that restricted the children action types to be unique. This change was needed for allowing multiple "submit" action types.

--- Sparx/src/java/main/com/netspective/sparx/report/tabular/HtmlReportActions.java
+++ Sparx/src/java/main/com/netspective/sparx/report/tabular/HtmlReportActions.java
@@ -1,45 +1,33 @@
 package com.netspective.sparx.report.tabular;
 
 import com.netspective.commons.xdm.XmlDataModelSchema;
+import com.netspective.sparx.navigate.NavigationContext;
 
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * Class for html report actions
  *
- * @author aye
- * $Id: HtmlReportActions.java,v 1.3 2003-09-02 17:15:54 roque.hernandez Exp $
+ * $Id: HtmlReportActions.java,v 1.4 2004-06-23 15:15:52 aye.thu Exp $
  */
 public class HtmlReportActions
 {
     public static final XmlDataModelSchema.Options XML_DATA_MODEL_SCHEMA_OPTIONS = new XmlDataModelSchema.Options().setIgnorePcData(true);
 
     private List actions = new ArrayList();
-    private Map actionsMap =  new HashMap();
 
     public HtmlReportActions()
     {
     }
 
     /**
-     * Gets the all the actions as a list
-     * @return
-     */
-    public List getActions()
-    {
-        return actions;
-    }
-
-    public Map getActionMap()
-    {
-        return actionsMap;
-    }
-
-    /**
-     * Gets a new html report action
+     * Creates a new generic html report action
+     *
      * @return
      */
     public HtmlReportAction createAction()
@@ -47,29 +35,71 @@ public class HtmlReportActions
         return new HtmlReportAction();
     }
 
-    public void setActions(List actions)
+    /**
+     * Creates a specific report action
+     *
+     * @param type
+     * @return
+     */
+    public HtmlReportAction createAction(int type)
     {
-        this.actions = actions;
+        HtmlReportAction action = new HtmlReportAction();
+        action.setType(new HtmlReportAction.Type(type));
+        return action;
     }
 
     /**
      * Gets a report action by index
-     * @param type
+     *
+     * @param index
      * @return
      */
-    public HtmlReportAction get(int type)
+    public HtmlReportAction getByIndex(int index)
     {
-        return (HtmlReportAction) actions.get(type);
+        return (HtmlReportAction) actions.get(index);
     }
 
     /**
-     * Gets a report action by value
+     * Gets report actions based on their type
+     *
+     * @param type  requested report action type
+     * @return      an array of report actions of the requested type
+     */
+    public HtmlReportAction[] getByType(int type)
+    {
+        List newList = new ArrayList();
+        int listSize = actions.size();
+        for (int i=0; i < listSize; i++)
+        {
+            HtmlReportAction action = (HtmlReportAction) actions.get(i);
+            if (action.getType().getValueIndex() == type)
+                newList.add(action);
+        }
+
+        int newListSize = newList.size();
+        HtmlReportAction[] actionList = new HtmlReportAction[newListSize];
+        for (int j=0; j < newListSize; j++)
+        {
+            actionList[j] = (HtmlReportAction) newList.get(j);
+        }
+        return actionList;
+    }
+
+    /**
+     * Gets all the actions as an array
+     *
      * @return
      */
-    public HtmlReportAction get(String value)
+    public HtmlReportAction[] getAll()
     {
-        return (HtmlReportAction) actionsMap.get(value);
+        HtmlReportAction[] actionList = new HtmlReportAction[actions.size()];
+        for (int i=0; i < actionList.length; i++)
+        {
+            actionList[i] = (HtmlReportAction) actions.get(i);
+        }
+        return actionList;
     }
+
 
     /**
      * Adds a report action using its type as the index
@@ -78,11 +108,10 @@ public class HtmlReportActions
     public void addAction(HtmlReportAction action)
     {
         actions.add(action);
-        actionsMap.put(action.getType().getValue(), action);
     }
 
     public int size()
     {
         return actions.size();
-    }
+    }    
 }
