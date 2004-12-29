99c96846e0450e818e8dc5f30e4544470270e2a5
jeremy.d.hulick <jeremy.d.hulick>
2004-12-29 14:57:30 +0000

    DDL formats class for DB2.

---
+++ Axiom/src/java/main/com/netspective/axiom/policy/ddl/Db2SqlDdlFormats.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2000-2004 Netspective Communications LLC. All rights reserved.
+ *
+ * Netspective Communications LLC ("Netspective") permits redistribution, modification and use of this file in source
+ * and binary form ("The Software") under the Netspective Source License ("NSL" or "The License"). The following
+ * conditions are provided as a summary of the NSL but the NSL remains the canonical license and must be accepted
+ * before using The Software. Any use of The Software indicates agreement with the NSL.
+ *
+ * 1. Each copy or derived work of The Software must preserve the copyright notice and this notice unmodified.
+ *
+ * 2. Redistribution of The Software is allowed in object code form only (as Java .class files or a .jar file
+ *    containing the .class files) and only as part of an application that uses The Software as part of its primary
+ *    functionality. No distribution of the package is allowed as part of a software development kit, other library,
+ *    or development tool without written consent of Netspective. Any modified form of The Software is bound by these
+ *    same restrictions.
+ *
+ * 3. Redistributions of The Software in any form must include an unmodified copy of The License, normally in a plain
+ *    ASCII text file unless otherwise agreed to, in writing, by Netspective.
+ *
+ * 4. The names "Netspective", "Axiom", "Commons", "Junxion", and "Sparx" are trademarks of Netspective and may not be
+ *    used to endorse or appear in products derived from The Software without written consent of Netspective.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED REPRESENTATIONS AND
+ * WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT,
+ * ARE HEREBY DISCLAIMED.
+ *
+ * NETSPECTIVE AND ITS LICENSORS SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY LICENSEE OR ANY THIRD PARTY AS A
+ * RESULT OF USING OR DISTRIBUTING THE SOFTWARE. IN NO EVENT WILL NETSPECTIVE OR ITS LICENSORS BE LIABLE FOR ANY LOST
+ * REVENUE, PROFIT OR DATA, OR FOR DIRECT, INDIRECT, SPECIAL, CONSEQUENTIAL, INCIDENTAL OR PUNITIVE DAMAGES, HOWEVER
+ * CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY, ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE, EVEN
+ * IF IT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+ */
+package com.netspective.axiom.policy.ddl;
+
+import com.netspective.commons.xdm.XmlDataModelSchema;
+
+public class Db2SqlDdlFormats extends AnsiSqlDdlFormats
+{
+    public static final XmlDataModelSchema.Options XML_DATA_MODEL_SCHEMA_OPTIONS = new XmlDataModelSchema.Options().setIgnorePcData(true);
+
+    public Db2SqlDdlFormats()
+    {
+        super();
+        setCreateSequenceStatementFormat("CREATE SEQUENCE ${column.sequenceName} AS INT START WITH 1 INCREMENT BY 1 MINVALUE 1 NO MAXVALUE NO CYCLE NO CACHE ORDER");
+        setTableCommentClauseFormat("COMMENT ON TABLE ${table.name} IS " +
+                                    "${textUtils.createLiteral(table.description, \"'\", \"'\", \"''\", true, true, " +
+                                    "\"'No description provided.'\")}");
+        setColumnCommentClauseFormat("COMMENT ON COLUMN ${column.qualifiedName} IS " +
+                                     "${textUtils.createLiteral(column.descr, \"'\", \"'\", \"''\", true, true, " +
+                                     "\"'No descr provided.'\")}");
+        setCreatePrimaryKeyIndex(false);
+        setBlankLineAllowedInCreateTable(false);
+    }
+}
