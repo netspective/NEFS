f4726bfc76192aed48a30822550c2d1c953df655
shahid.shah <shahid.shah>
2005-06-29 18:48:45 +0000

    Reverse DB2/6000 and DB2/NT since it seemed to not work the other way around.

--- Axiom/src/java/main/com/netspective/axiom/policy/Db2DatabasePolicy.java
+++ Axiom/src/java/main/com/netspective/axiom/policy/Db2DatabasePolicy.java
@@ -46,7 +46,7 @@ public class Db2DatabasePolicy extends AnsiDatabasePolicy
     public Db2DatabasePolicy()
     {
         setName("db2");
-        setAliases("DB2/NT,DB2/6000");
+        setAliases("DB2/6000,DB2/NT");
     }
 
     public SqlDdlFormats createDdlFormats()
